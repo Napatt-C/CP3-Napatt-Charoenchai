cae390d700531a5ab220f512b03c9760646e4582
Napatt-C <85002870+Napatt-C@users.noreply.github.com>
2021-11-05 14:14:54 +0700

    Exercise4
    student score from 4 subjects

---
+++ Exercise4_Napatt_C.py
@@ -0,0 +1,10 @@
+found_eng = 80
+gen_bu = 75.3
+intro_com = 97.5
+com_pro = 63.3
+
+print("--- Your Score ---")
+print("Foundation English :", found_eng)
+print("General Business :", gen_bu)
+print("Introduction to Computer Systems :", intro_com)
+print("Computer Programing :", com_pro)
