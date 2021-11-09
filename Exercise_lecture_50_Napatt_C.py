8ae71d764d6f735476783c43238f2b213c052c90
Napatt-C <85002870+Napatt-C@users.noreply.github.com>
2021-11-09 11:24:44 +0700

    Exercise_Lecture50_Napatt_C

---
+++ Exercise_lecture_50_Napatt_C.py
@@ -0,0 +1,17 @@
+def plusNum(x, y):
+    print(x+y)
+
+def minusNum(x, y):
+    print(x-y)
+
+def multiNum(x, y):
+    print(x*y)
+
+def divideNum(x, y):
+    print(x/y)
+
+plusNum(5, 10)
+minusNum(5, 10)
+multiNum(5, 10)
+divideNum(5, 10)
+
