896edf394ef49f5a7254640139471654c6124480
Napatt-C <85002870+Napatt-C@users.noreply.github.com>
2021-11-09 09:37:37 +0700

    edit ast (line4)

--- Exercise_11_Napatt_C.py
+++ Exercise_11_Napatt_C.py
@@ -1,8 +1,7 @@
 user_input = int(input("Number :"))
 
 for i in range(user_input):
-    ast = "*"
-    create_aster = ast * (2 * i + 1)
+    create_aster = "*" * (2 * i + 1)
     space_ast = " " * (user_input - (i+1))
     print(space_ast, create_aster)
 
